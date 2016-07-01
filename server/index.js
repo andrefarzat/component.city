cf6051300c9d379b099ecd1d777dd0daa9d2a302
Andre Farzat <andrefarzat@gmail.com>
2016-07-01 15:54:25 -0400

    Creating a good directory structure

--- server/index.js
+++ server/index.js
@@ -2,17 +2,25 @@ var express = require('express');
 var swig    = require('swig');
 var path    = require('path');
 
+const ROOT_DIR      = path.join(__dirname, '..');
+const TEMPLATES_DIR = path.join(ROOT_DIR, 'templates');
+const STATIC_FOLDER = path.join(ROOT_DIR, 'public');
+
 var app = express();
 
 // Configurations
 app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
 app.set('view cache', false);
-app.set('views', path.join(__dirname, '..', 'pages'));
+app.set('views', TEMPLATES_DIR);
 swig.setDefaults({ cache: false });
 
+// Static folder
+
+app.use('/static', express.static(STATIC_FOLDER));
+
 // Pages
-app.get('/', require('./home'));
+app.get('/', require('./pages/home'));
 
 // Staring the server
 app.listen(3000, function(){
