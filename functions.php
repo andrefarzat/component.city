dc0a11692ae3d402b00e63315e76a9f78bdf81e9
Andre Farzat <andrefarzat@gmail.com>
2016-11-25 22:44:02 -0500

    Creating the router

--- functions.php
+++ functions.php
@@ -1,7 +1,115 @@
 <?php
 
+define('CITY_COMPONENTS_FOLDER', '.' . DIRECTORY_SEPARATOR . 'components');
 
+/**
+ * @see http://stackoverflow.com/a/834355/1327401
+ */
+function startsWith($haystack, $needle)
+{
+     $length = strlen($needle);
+     return (substr($haystack, 0, $length) === $needle);
+}
 
+/**
+ * @see http://stackoverflow.com/a/834355/1327401
+ */
+function endsWith($haystack, $needle)
+{
+    $length = strlen($needle);
+    if ($length == 0) {
+        return true;
+    }
 
+    return (substr($haystack, -$length) === $needle);
+}
+
+/**
+ * The following function will strip the script name from URL
+ * i.e.  http://www.something.com/search/book/fitzgerald will become /search/book/fitzgerald
+ * @see http://blogs.shephertz.com/2014/05/21/how-to-implement-url-routing-in-php/
+ * @return {string}
+ */
+function getCurrentUri()
+{
+    $basepath = implode('/', array_slice(explode('/', $_SERVER['SCRIPT_NAME']), 0, -1)) . '/';
+    $uri = substr($_SERVER['REQUEST_URI'], strlen($basepath));
+    if (strstr($uri, '?')) $uri = substr($uri, 0, strpos($uri, '?'));
+    $uri = '/' . trim($uri, '/');
+    return $uri;
+}
+
+/**
+ * @return {string[]}
+ */
+function getAllComponents()
+{
+    $components = array();
+    $dirs       = scandir(CITY_COMPONENTS_FOLDER);
+
+    foreach($dirs as $dir) {
+        if (! startsWith($dir, '.')) {
+            array_push($components, $dir);
+        }
+    }
+
+    return $components;
+}
+
+/**
+ * @param {string} $folderName
+ * @return {object}
+ */
+function generateComponentObjectFromFolder($folderName)
+{
+    $dirPath = CITY_COMPONENTS_FOLDER . DIRECTORY_SEPARATOR . $folderName;
+
+    if (! is_dir($dirPath)) {
+        throw new Exception("Component $folderName doesn't exist!");
+    }
+
+    $files = scandir($dirPath);
+    $component = array(
+        'path'            => $dirPath,
+        'implementations' => array(),
+        'hasCss'          => 0,
+        'errors'          => array(),
+    );
+
+    foreach($files as $file) {
+        if (startsWith($file, '.')) {
+            continue;
+        }
+
+        if (endsWith($file, '.css')) {
+            if ($file != 'style.css') {
+                $msg = "There can be only 'style.css' as css file. Please, remove '$file' from '$dirPath'.";
+                array_push($component['errors'], $msg);
+            } else {
+                $compontent['hasCss'] = 1;
+            }
+            continue;
+        }
+
+        if (endsWith($file, '.html')) {
+            array_push($component['implementations'], $file);
+            continue;
+        }
+
+        $msg = "File '$file' is unnecessary. Please, remove it.";
+        array_push($component['errors'], $msg);
+    }
+
+    return $component;
+}
+
+/**
+ * @param {string} $componentName
+ * @return {string}
+ */
+function formatComponentUrl($componentName)
+{
+    return $componentName;
+}
 
 ?>
---
+++ home.php
@@ -0,0 +1,33 @@
+<?php require_once('functions.php'); ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Component City</title>
+    <meta charset="UTF-8" />
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet" />
+    <link rel="stylesheet" href="/css/style.css" />
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+
+                <h1 class="hugefuckinglogo">
+                    <span class="hugefuckinglogo-image"></span>
+                    Component.City
+                </h1>
+
+                <div class="spacer +big"></div>
+
+                <ol class="list +inline">
+                    <?php foreach(getAllComponents() as $component): ?>
+                        <li class="list-item">
+                            <a href="/<?php echo formatComponentUrl($component); ?>" class="linkpill"><?php echo $component; ?></a>
+                        </li>
+                    <?php endforeach; ?>
+                </ol>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
